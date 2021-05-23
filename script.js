967732ed680c18de01b483206151a5af67eba1fc
Harsh Kundnani <harshkundnani@Harshs-MacBook-Air.local>
2021-05-23 02:47:07 -0400

    Add overlay background and animation

---
+++ script.js
@@ -0,0 +1,15 @@
+const btnMenu = document.querySelector("#btnMenu");
+const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
+
+btnMenu.addEventListener("click", function () {
+  if (header.classList.contains("open")) {
+    header.classList.remove("open");
+    overlay.classList.remove("fade-in");
+    overlay.classList.add("fade-out");
+  } else {
+    header.classList.add("open");
+    overlay.classList.remove("fade-out");
+    overlay.classList.add("fade-in");
+  }
+});
