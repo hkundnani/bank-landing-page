f0db76f6dd2a55908d648e27f173081aafa21189
Harsh Kundnani <hkundnani@gmail.com>
2021-05-24 16:04:13 -0400

    Add header_menu and animation

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 const btnMenu = document.querySelector("#btnMenu");
 const header = document.querySelector(".header");
 const overlay = document.querySelector(".overlay");
+const fadeElems = document.querySelectorAll(".has-fade");
+const body = document.querySelector("body");
 
 btnMenu.addEventListener("click", function () {
   if (header.classList.contains("open")) {
+    body.classList.remove("no-scroll");
     header.classList.remove("open");
-    overlay.classList.remove("fade-in");
-    overlay.classList.add("fade-out");
+    fadeElems.forEach(function (element) {
+      element.classList.remove("fade-in");
+      element.classList.add("fade-out");
+    });
   } else {
+    body.classList.add("no-scroll");
     header.classList.add("open");
-    overlay.classList.remove("fade-out");
-    overlay.classList.add("fade-in");
+    fadeElems.forEach(function (element) {
+      element.classList.remove("fade-out");
+      element.classList.add("fade-in");
+    });
   }
 });
